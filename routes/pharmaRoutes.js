99dfb14e8915dc787e638adc1beab836e9d7e1b9
apple <giteshshastri96@gmail.com>
2019-02-03 18:54:17 +0530

    Added Email

--- routes/pharmaRoutes.js
+++ routes/pharmaRoutes.js
@@ -55,6 +55,28 @@ router.post('/new', (req, res, next) => {
     });
 });
 
+router.get('/updatePharma', (req, res, next) => {
+    Person.findOne({_id:req.query.id})
+    .populate('Allocated_Pharma')
+    .populate('user')
+    .exec()
+    .then(doc=> {  
+        Pharmacy.update({_id: doc.Allocated_Pharma._id}, {$set: {contact: req.query.phone, email: req.query.email, area: req.query.area}})
+        .exec(); 
+        User.update({_id: doc.user._id}, {$set: {phone: req.query.phone}})
+        .exec();    
+        Person.update({_id: doc._id}, {$set: {Allocated_Area: req.query.area}})
+        .exec();   
+        res.status(200).json({message: 'updated', data: doc});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+
 router.post('/update/:pharmaId', (req, res, next) => {
     const id = req.params.pharmaId;
     const updateOps = {};
