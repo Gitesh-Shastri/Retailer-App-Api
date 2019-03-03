4be26cd528cdddf75cea7f557180f18dd5a9f1ad
Gitesh-Shastri <40106556+Gitesh-Shastri@users.noreply.github.com>
2019-03-03 16:43:11 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -385,11 +385,11 @@ router.get('/updateApp', (req, res, next) => {
                 code: doc[0].code,
                 count: doc[0].count,
                 "Version": [{
-                    "version": "2.1.9",
+                    "version": "2.2.0",
                     "error": "01"
                 }],
                 "Controle": [{
-                    "version": "2.1.9",
+                    "version": "2.2.0",
                     "error": "01"
                 }]
             })
